766c2effb0053c1b336cac76a2c6ce7bdabf8071
hedaniel <d@nielhe.com>
2022-04-15 15:45:17 -0700

    more

--- register_confirmation.php
+++ register_confirmation.php
@@ -1,13 +1,62 @@
+<?php
+require 'config/config.php';
+
+var_dump($_POST);
+
+if (
+    !isset($_POST['email']) || empty($_POST['email'])
+    || !isset($_POST['username']) || empty($_POST['username'])
+    || !isset($_POST['password']) || empty($_POST['password'])
+) {
+    $error = "Please fill out all required fields.";
+} else {
+
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    if ($mysqli->connect_errno) {
+        echo $mysqli->connect_error;
+        exit();
+    }
+
+    $statement_registered = $mysqli->prepare("SELECT * FROM users WHERE username=? OR email=?");
+    $statement_registered->bind_param("ss", $_POST["username"], $_POST["email"]);
+    $execute_registered = $statement_registered->execute();
+
+    if (!$execute_registered) {
+        echo $mysqli->error;
+    }
+
+    $statement_registered->store_result();
+
+    if ($statement_registered->num_rows > 0) {
+        $error = "Username or Email already used!";
+        $statement_registered->close();
+    } else {
+        $password = hash("sha256", $_POST["password"]);
+
+        $statement = $mysqli->prepare("INSERT INTO users(username, email, password) VALUES (?,?,?)");
+
+        $statement->bind_param("sss", $_POST["username"], $_POST["email"], $password);
+
+        $executed = $statement->execute();
+
+        if (!$executed) {
+            echo $mysqli->error;
+        }
+
+        $statement->close();
+    }
+}
+?>
+
 <head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="navbar-styles.css">
     <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="login-styles.css"> 
-    <meta charset="UTF-8">   
+    <link rel="stylesheet" href="login-styles.css">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
@@ -32,7 +81,7 @@
                 <div class="icon">😉</div>
             </div>
         </div>
-    </div> 
+    </div>
     <div class="container">
         <div class="login-wrapper">
             <div class="bagel-hero">Bagel 🍩</div>
@@ -41,13 +90,10 @@
         <div class="line-vertical"></div>
         <div class="line-horizontal"></div>
         <div class="login-wrapper fade-in">
-            <div class="login-text">Welcome Back.</div>
-            <input type="text" class="login-input username box-shadow" placeholder="username">
-            <input type="text" class="login-input password box-shadow" placeholder="password">
-            <button class="login">Let's Go! 🍩</button>
+            <div class="login-text">Create Account.</div>
+            <div class="create-text success">Successfully created!</div>
+            <button class="login" onclick="location.href = 'login.html';">Login! </button>
             <div class="login-options">
-                <a href="register_form.php" class="text">i'm new!</a>
-                <a class="text">forgot password?</a>
             </div>
         </div>
     </div>
--- register_form.php
+++ register_form.php
@@ -1,13 +1,11 @@
 <head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="navbar-styles.css">
     <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="login-styles.css"> 
-    <meta charset="UTF-8">   
+    <link rel="stylesheet" href="login-styles.css">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
@@ -32,25 +30,32 @@
                 <div class="icon">😉</div>
             </div>
         </div>
-    </div> 
-    <div class="container">
-        <div class="login-wrapper">
-            <div class="bagel-hero">Bagel 🍩</div>
-            <div class="author-text">// by Daniel He</div>
-        </div>
-        <div class="line-vertical"></div>
-        <div class="line-horizontal"></div>
-        <div class="login-wrapper fade-in">
-            <div class="login-text">Welcome Back.</div>
-            <input type="text" class="login-input username box-shadow" placeholder="username">
-            <input type="text" class="login-input password box-shadow" placeholder="password">
-            <button class="login">Let's Go! 🍩</button>
-            <div class="login-options">
-                <a href="register_form.php" class="text">i'm new!</a>
-                <a class="text">forgot password?</a>
+    </div>
+
+    <form action="register_confirmation.php" method="POST">
+
+        <div class="container">
+            <div class="login-wrapper">
+                <div class="bagel-hero">Bagel 🍩</div>
+                <div class="author-text">// by Daniel He</div>
+            </div>
+            <div class="line-vertical"></div>
+            <div class="line-horizontal"></div>
+            <div class="login-wrapper fade-in">
+                <div class="login-text">Create Account.</div>
+
+                <input type="text" class="login-input username box-shadow" placeholder="username" name="username">
+                <input type="text" class="login-input password box-shadow" placeholder="password" name="password">
+
+                <button type="submit" class="login">Register! 😎</button>
+                <div class="login-options">
+                    <a href="login.html" class="text">
+                        << sign in </a>
+                </div>
             </div>
         </div>
-    </div>
+
+    </form>
     <!-- <button class="box-shadow" id="addInputButton" type="button">+</button>
     <button class="box-shadow" id="infoButton" type="button">?</button> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
